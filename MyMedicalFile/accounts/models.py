512fb83e539e35fd6f580c20457d626ea75d4d87
NjoudAlhazmi <njoudz19@gmail.com>
2023-02-24 18:04:57 +0300

    complate steps

--- MyMedicalFile/accounts/models.py
+++ MyMedicalFile/accounts/models.py
@@ -9,13 +9,19 @@ from django.dispatch import receiver
 class Profile(models.Model):
     user = models.OneToOneField(User,on_delete=models.CASCADE)
     phone_number = models.CharField(max_length=10 , null=True , blank=True)
+    addres = models.CharField(max_length=50 , blank=True , null=True)
+
+    # image
+    # age
+    # job
+    
 
     def __str__(self):
         return str(self.user)
 
-    @receiver(post_save , sender=User)
-    def create_user_profile(sender,instance,created , **kwargs):
-        if created:
-            Profile.objects.create(
-                user = instance
+@receiver(post_save , sender=User)
+def create_user_profile(sender,instance,created , **kwargs):
+    if created:
+        Profile.objects.create(
+            user = instance
             )
